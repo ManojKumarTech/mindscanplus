25fc3c29b0c388f6546e8093c17751b47c46d7cf
ManojKumar <mano22056.it@rmkec.ac.in>
2026-03-14 15:33:34 +0530

    feat: add utility constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+/**
+ * Admin UIDs – add Firebase UIDs here to grant admin access to the Admin Dashboard.
+ * This list is shared between Navbar and Admin page.
+ */
+export const ADMIN_UIDS: string[] = [
+  'dNDk5w5QYwOhiEufVHuOsEp6T472',
+  'vOdYfb2pbcQjED7APCgsieoLHiE2',
+  'CuoEClKLcWfWgdxfPV5HHvsyfmv1',
+  'GwtOxqRTf0aoD5FOOHvY3R55hCa2',
+];
